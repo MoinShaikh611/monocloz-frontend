30db16a68ee055adb30f1d4e135c6a2baf23c6b4
MoinShaikh611 <imoin611@gmail.com>
2023-06-21 23:50:44 +0530

    add to bag

--- components/AddToCartButton/AddToCartButton.tsx
+++ components/AddToCartButton/AddToCartButton.tsx
@@ -1,8 +1,13 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useCart } from "contexts/CartContext";
+import { useState } from "react";
 
 export const AddToCartButton: React.FC<{ product: any }> = ({ product }) => {
   const { addToCart } = useCart();
+  const router = useRouter();
+  const [addedToCart, setAddedToCart] = useState(false);
+
   const handleAddToCart = () => {
     const cartItem = {
       productId: product._id,
@@ -10,12 +15,21 @@ export const AddToCartButton: React.FC<{ product: any }> = ({ product }) => {
       name: product.name,
       price: product.price,
     };
+
     addToCart(cartItem);
+    setAddedToCart(true);
+  };
+
+  const handleGoToBag = () => {
+    router.push("/cart"); // Redirect to the cart page
   };
 
   return (
-    <button onClick={handleAddToCart} className="add-to-cart-btn w-100">
-      Add to Cart
+    <button
+      onClick={addedToCart ? handleGoToBag : handleAddToCart}
+      className="add-to-cart-btn w-100"
+    >
+      {addedToCart ? "Go to Bag" : "Add to Cart"}
     </button>
   );
 };
